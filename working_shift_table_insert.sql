d20fde4089d2f0aa1ba9bb23c22b84031c083247
miyasan31 <miyasan.19990301@gmail.com>
2021-09-15 12:39:12 +0900

    🔥

--- working_shift_table_insert.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `working_shift` (
-  `working_shift_id` int(5) NOT NULL AUTO_INCREMENT,
-  `employee_id` int(5) NOT NULL,
-  `working_shift_join` datetime NOT NULL,
-  `working_shift_leaving` datetime,
-  PRIMARY KEY(`working_shift_id`),
-  FOREIGN KEY(`employee_id`)
-    REFERENCES employee (`employee_id`)
-) DEFAULT CHARSET=utf8;
